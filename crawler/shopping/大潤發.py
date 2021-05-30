424ab1d59785c5101427532218795f548ac284c0
virness <78792273+virness@users.noreply.github.com>
2021-05-30 16:20:07 +0800

    Update 大潤發.py

--- crawler/shopping/大潤發.py
+++ crawler/shopping/大潤發.py
@@ -12,41 +12,41 @@ soup = BeautifulSoup(rsp.text, 'lxml')
 
 try:
     if resp.status_code == 200:
-    for li in soup.select('li.nav01 h4 a'):
-    data=[]
-    url = 'https://www.rt-mart.com.tw/direct/'+li.get('href')
-    res=requests.get(url)
-    soup = BeautifulSoup(res.text, 'lxml')
-    for item in soup.select('h5.for_proname a'):
-    url = item.get('href')
-    res=requests.get(url)
-    soup = BeautifulSoup(res.text, 'lxml')
-    title=soup.select('span.h2')[0].text
-    price=soup.select('span.price_num')[0].text
-    content=soup.select('div.TxtContent')[0].text
-    data.append([title,price,content])
-    sleep_time = random.randint(3,10)
-    print("sleep time: %s sec"%(sleep_time))
-    time.sleep(sleep_time)
+        for li in soup.select('li.nav01 h4 a'):
+        data=[]
+        url = 'https://www.rt-mart.com.tw/direct/'+li.get('href')
+        res=requests.get(url)
+        soup = BeautifulSoup(res.text, 'lxml')
+        for item in soup.select('h5.for_proname a'):
+        url = item.get('href')
+        res=requests.get(url)
+        soup = BeautifulSoup(res.text, 'lxml')
+        title=soup.select('span.h2')[0].text
+        price=soup.select('span.price_num')[0].text
+        content=soup.select('div.TxtContent')[0].text
+        data.append([title,price,content])
+        sleep_time = random.randint(3,10)
+        print("sleep time: %s sec"%(sleep_time))
+        time.sleep(sleep_time)
 
-    for li in soup.select('li.nav02 h4 a'):  
-            data1=[]  
-            url = 'https://www.rt-mart.com.tw/direct/'+li.get('href')  
-            res=requests.get(url)  
-            soup = BeautifulSoup(res.text, 'lxml')  
-            for item in soup.select('h5.for_proname a'):  
-                url = item.get('href')  
+        for li in soup.select('li.nav02 h4 a'):  
+                data1=[]  
+                url = 'https://www.rt-mart.com.tw/direct/'+li.get('href')  
                 res=requests.get(url)  
                 soup = BeautifulSoup(res.text, 'lxml')  
-                title=soup.select('span.h2')[0].text  
-                price=soup.select('span.price_num')[0].text  
-                content=soup.select('div.TxtContent')[0].text  
-                data1.append([title,price,content])  
-                sleep_time = random.randint(3,10)  
-                print("sleep time: %s sec"%(sleep_time))  
-                time.sleep(sleep_time)  
+                for item in soup.select('h5.for_proname a'):  
+                    url = item.get('href')  
+                    res=requests.get(url)  
+                    soup = BeautifulSoup(res.text, 'lxml')  
+                    title=soup.select('span.h2')[0].text  
+                    price=soup.select('span.price_num')[0].text  
+                    content=soup.select('div.TxtContent')[0].text  
+                    data1.append([title,price,content])  
+                    sleep_time = random.randint(3,10)  
+                    print("sleep time: %s sec"%(sleep_time))  
+                    time.sleep(sleep_time)  
 except:
-pass
+    pass
 columns = ['商品名稱','價格','商品介紹']
 df = pd.DataFrame(data=[data,data1],columns=columns)
 df.to_csv('Market.csv',index=False,encoding='utf-8-sig')
