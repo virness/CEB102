1b6b2a9991fd356007f94b85365c7a7241a7faed
virness <78792273+virness@users.noreply.github.com>
2023-03-28 20:18:55 +0800

    Update app.py

--- chef/app.py
+++ chef/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, render_template_string
-import openai
+import openai,os
+
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 app = Flask(__name__)
 
